c1492fb3afc8001d0fe42e34d4b8129b8db89d0b
gentle9828 <71665004+gentle9828@users.noreply.github.com>
2022-10-09 00:01:43 +0900

    update

--- Main.java
+++ Main.java
@@ -1,27 +1,5 @@
-import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        ArrayList<Integer> array = new ArrayList<>();
-        st = new StringTokenizer(br.readLine());
-        if(st.hasMoreTokens()) {
-            array.add(Integer.parseInt(st.nextToken()));
-        }
-
-
-        System.out.println(array);
-        for(int i = 0; i < array.size(); i++) {
-            bw.write(array.get(i));
-            bw.flush();
-        }
-
-        br.close();
-        bw.close();
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
---
+++ jiSang.java
@@ -0,0 +1,5 @@
+public class jiSang {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
